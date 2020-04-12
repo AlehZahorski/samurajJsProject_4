82726331b5f31e84c7218acb5752d556f598a6c8
AlehZahorski <git3371m@gmail.com>
2020-04-12 09:52:10 +0200

    review code part 2

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
     //.clientX(ekran),.pageY(strona),.screenX(monitor)
 document.body.addEventListener('mousemove', (event) => {
-    document.querySelector('h2').textContent = `${event.clientX}, ${event.clientY}`;
-    document.body.style.backgroundColor = `rgb(${event.clientX/3},
-    ${event.clientY/2.5}, ${event.clientY/event.clientX * 10})`;
+    const hTwoOutput = document.querySelector('h2');
+    const x = event.clientX;
+    const y = event.clientY;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+
+    const firstColor = `${x/width*100}`;
+    const secondColor = `${y/height*100}`;
+    const thirdColor = `${((x/width*100)+(y/height*100))/2}`;
+
+
+    hTwoOutput.textContent = `${x}, ${y}`;
+    document.body.style.backgroundColor = `rgb(${firstColor}%,
+    ${secondColor}%, ${thirdColor}%)`;
+
 })
 
