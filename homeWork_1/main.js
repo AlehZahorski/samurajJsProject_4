d4e576bfea11ada0221b1149394dd0760645558d
AlehZahorski <git3371m@gmail.com>
2020-04-12 10:55:06 +0200

    review code part three

--- homeWork_1/main.js
+++ homeWork_1/main.js
@@ -12,3 +12,25 @@ document.body.addEventListener('click', (event) => {
     console.log(`${x}, ${y}`)
  }
 })
+
+
+
+// document.body.addEventListener('click', (event) => {
+// const color = getColor(event);
+// document.body.style.backgroundColor = color;
+// })
+
+// function getColor(e) {
+//     let x = event.clientX;
+//     let y = event.clientY;
+//     if(x%2==0 && y%2==0){
+//         console.log(`${x}, ${y}`);
+//         return "red";
+//     }else if(x%2==1 && y%2==1){
+//        console.log(`${x}, ${y}`)
+//        return "blue";
+//     }else{
+//        console.log(`${x}, ${y}`)
+//        return "green";
+//     }
+//    }
